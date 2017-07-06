0988854431bcc1f1b91dcc98a88cd9a7c5b0a3a1
mikado <g1527066@st.t-kougei.ac.jp>
2017-07-06 20:27:05 +0900

    エラーが起きてない？

--- project/project/ProgrammingBattle/Assets/Scripts/TextManagement.cs
+++ project/project/ProgrammingBattle/Assets/Scripts/TextManagement.cs
@@ -18,7 +18,7 @@ public class TextManagement
     GameObject enemyDescription;//選択した技説明
     GameObject inputTechnique;//選択した技説明
     GameObject inputAnswer;//選択した技説明
-    
+
     string chooseTechnique = "ChooseTechnique";
     string chooseEnemy = "ChooseEnemy";
 
@@ -38,24 +38,35 @@ public class TextManagement
 
         //技や敵のように数が変わるもの以外の生成
         //chooseTechniqueの文字生成
-        techniqueObject.Add(Generation(text, new Vector3(-200, -245, 0),8,chooseTechnique,Color.black));
+        techniqueObject.Add(Generation(text, new Vector3(170, -600, 0)/*+Canvas.transform.localPosition*/, 24, chooseTechnique, Color.black));
         //選択中の技の説明
-        enemyDescription=Generation(text, new Vector3(80, -275, 0), 8, "技説明", Color.black);
+        enemyDescription = Generation(text, new Vector3(80, -275, 0), 22, "技説明", Color.black);
         //chooseEnemyの文字生成
-        enemyObjectt.Add(Generation(text, new Vector3(80, -245, 0), 8, chooseEnemy, Color.clear));
+        enemyObjectt.Add(Generation(text, new Vector3(80, -245, 0), 22, chooseEnemy, Color.clear));
         //入力文字、答生成
-        inputTechnique = Generation(text, new Vector3(180, -175, 0), 12, "input", Color.black);
-        inputAnswer = Generation(text, new Vector3(180, -175, 0), 12, "in", Color.red);
-
+        inputTechnique = Generation(text, new Vector3(180, -175, 0), 25, "input", Color.black);
+        inputAnswer = Generation(text, new Vector3(180, -175, 0), 25, "in", Color.red);
+        //技も基本３つなので生成しとく、二つ以下になったらcolorで見えなくする
+        int interval = -30;
+        techniqueObject.Add(Generation(text, new Vector3(150 - Canvas.transform.position.x, -630 + Canvas.transform.position.y, 0), 22, "技の名前1", Color.black));
+        techniqueObject.Add(Generation(text, new Vector3(-200, -245 + interval * 2, 0), 22, chooseTechnique, Color.black));
+        techniqueObject.Add(Generation(text, new Vector3(-200, -245 + interval * 3, 0), 22, chooseTechnique, Color.black));
+        //敵名生成最大５体こちらもcolorで制御する
+        enemyObjectt.Add(Generation(text, new Vector3(80, -245 + interval * 1, 0), 22, "敵の名前1", Color.black));
+        enemyObjectt.Add(Generation(text, new Vector3(80, -245 + interval * 2, 0), 22, chooseEnemy, Color.black));
+        enemyObjectt.Add(Generation(text, new Vector3(80, -245 + interval * 3, 0), 22, chooseEnemy, Color.black));
+        enemyObjectt.Add(Generation(text, new Vector3(80, -245 + interval * 4, 0), 22, chooseEnemy, Color.black));
+        enemyObjectt.Add(Generation(text, new Vector3(80, -245 + interval * 5, 0), 22, chooseEnemy, Color.black));
     }
 
     //オブジェクトを生成します
-    GameObject Generation(GameObject prefab, Vector3 position,int fontSize,string drawString,Color color)
+    GameObject Generation(GameObject prefab, Vector3 position, int fontSize, string drawString, Color color)
     {
         GameObject ob = GameObject.Instantiate(prefab) as GameObject;
-       
+
         ob.transform.parent = Canvas.transform;
-        ob.transform.localPosition = position;
+        RectTransform rt = ob.GetComponent<RectTransform>();
+        rt.localPosition = position;
         txt = ob.GetComponent<Text>();
         txt.text = drawString;
         txt.fontSize = fontSize;
@@ -66,19 +77,19 @@ public class TextManagement
 
 
     //技のリスト選択項目を受け取って表示
-    public void SelectedTechnique(string[] TechniqueName,string selectDescription, int select, BattleSystemScript.BattleState nowState)
+    public void SelectedTechnique(string[] TechniqueName, string selectDescription, int select, BattleSystemScript.BattleState nowState)
     {
 
-        switch(nowState)
+        switch (nowState)
         {
             case BattleSystemScript.BattleState.ChooseWord:
                 if (oldState != nowState)
                 {
-                 //   oldState = nowState;
-                 //前回の技文字を消す
-                 for(int i=0;i<techniqueObject.Count-1;i++)
+                    //   oldState = nowState;
+                    //前回の技文字を消す
+                    for (int i = 0; i < techniqueObject.Count - 1; i++)
                     {
-                     //   if(techniqueObject[i]!=null)
+                        //   if(techniqueObject[i]!=null)
 
                     }
 
